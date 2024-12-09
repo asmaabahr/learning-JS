279c9fc11a0a16d4c4a9a2adb26bf2f08d4d182f
asmaa bahr <asmaa559632@gmail.com>
2024-12-09 23:53:28 +0200

    introduction

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-window.alert('')
+// ways to proceed an output
+
+//?Browser
+window.alert('Hello JS');//have priorty
+
+//?in HTML element
+document.getElementById('demo').innerHTML ="Hello JS"
+
+//?in Console
+console.log('hello console')
