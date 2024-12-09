cb0069c677a63436b8e1a6bdadc871c55d3ff5cd
asmaa bahr <asmaa559632@gmail.com>
2024-12-10 00:31:35 +0200

    var&datatypes

--- script.js
+++ script.js
@@ -1,10 +1,66 @@
 // ways to proceed an output
 
 //?Browser
-window.alert('Hello JS');//have priorty
+// window.alert('Hello JS');//have priorty
 
 //?in HTML element
 document.getElementById('demo').innerHTML ="Hello JS"
 
 //?in Console
 console.log('hello console')
+
+
+//variables
+//@ is container to store some value with name
+
+//variable name
+var userAge = 23;
+var $userAge = 23;//true
+var _userAge = 23;//true
+var userComment = "good morning";//true
+// var 3userAge = 23;//false
+// var user Age = 23;//false
+// var @userAge = 23;//false
+// var var = 23;//false
+
+
+//datatypes
+/*$ 
+
+loosly typed & dynamic typed
+
+loosly typed => didn't define var type before declare it
+dynamic typed => i can change data type while run type
+
+
+Primitive Datatypes
+-- Number(10,25.5,0.343)
+-- String("",'')->any thing inside them
+-- Boolean(true,false)
+-- Undefind(var without value)
+-- Null (structural root)
+
+ESC6
+--bigInt
+--symbol
+
+Structural Datatypes(non primtive)
+-- Object
+-- Function 
+
+*/
+
+
+// var personAge = 25;
+var personAge = 25.5;
+console.log(typeof(personAge));
+
+var personName = "asmaa";
+// var personName = "1234";
+console.log(typeof(personName));
+
+var isAdmin = false;
+console.log(typeof(isAdmin));
+
+var productPrice;
+console.log(typeof(productPrice));//undefind
