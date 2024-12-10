e8f65d22a5e801cbc1553faaf788cfa8441c5b88
asmaa bahr <asmaa559632@gmail.com>
2024-12-10 22:34:02 +0200

    prompt

--- script.js
+++ script.js
@@ -39,6 +39,7 @@ Primitive Datatypes
 -- Boolean(true,false)
 -- Undefind(var without value)
 -- Null (structural root)
+-- NaN (special number)
 
 ESC6
 --bigInt
@@ -96,4 +97,53 @@ console.log(b)//5555 string
 
 var c = "5"*"5";
 console.log(c);//25 number
+
+
+
+//prompt
+//?take data from user
+/*window.prompt("Enter your user name");*/
+
+//i can recive this input by adding prompt into var
+//------------------------------------
+// var prom = window.prompt("Enter your user name");
+// console.log(prom);
+//------------------------------------
+
+//program that takes 2 number from user, return its addition
+//------------------------------------
+// var num1 = prompt("enter first number");
+// var num2 = prompt("enter second number");
+// var result = num1 + num2;
+
+//window.alert(result);//it will concat the 2 numbers won't adding them
+//>prompt deal with any input to it as string so when adding it will concat
+//------------------------------------
+
+//Use method -->Number() >convert string or any type into number
+
+//-----------------------------------
+// var n1 = Number(prompt("enter first number"));
+// var n2 = Number(prompt("enter second number"));
+// var res =n1 + n2;
+
+// window.alert(res);
+//------------------------------------
+
+var t = Number(true);
+console.log(t);//1
+
+var t = Number(false);
+console.log(t);//0
+
+var t = Number("");
+console.log(t);//0
+
+var t = Number("as");
+console.log(t);//NaN => not a number =>if string is n't a number
+
+var t = Number("as");
+console.log(typeof(t));//number
+
+console.log(typeof(NaN));//NUMBER
 
